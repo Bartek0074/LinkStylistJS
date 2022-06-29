e8340c8c1a56e1363fbfca14efdb542975ea0d43
Bartek0074 <88652468+Bartek0074@users.noreply.github.com>
2022-06-29 23:08:28 +0200

    added js to change active elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const links = document.querySelectorAll('.links-box__link');
-const linksVariation = document.querySelectorAll('.links-variation-box__element')
+const linksVariationBox = document.querySelectorAll('.links-variation-box__element')
+const linksVariation = document.querySelectorAll('.links-variation-box__link')
 
 const linkHandle = (e) =>{
     links.forEach(link => {
@@ -7,16 +8,33 @@ const linkHandle = (e) =>{
     })
     e.target.classList.add('links-box__link--active')
 
-    linksVariation.forEach(link => {
+    linksVariationBox.forEach(link => {
         link.classList.remove('links-variation-box__element--active')
     })
     for(let i = 0; i < links.length; i++){
         if(links[i].classList.contains('links-box__link--active')){
-            linksVariation[i].classList.add('links-variation-box__element--active')
+            linksVariationBox[i].classList.add('links-variation-box__element--active')
+            linksVariation.forEach(link => {
+                link.classList.remove('links-variation-box__link--active')
+
+            })
+            linksVariationBox[i].firstElementChild.classList.add('links-variation-box__link--active')
         }
     }
 }
 
+const linkVariationHandle = (e) => {
+    linksVariation.forEach(link => {
+        link.classList.remove('links-variation-box__link--active')
+    })
+
+    e.target.classList.add('links-variation-box__link--active')
+}
+
 links.forEach(link => {
     link.addEventListener('click', linkHandle)
+})
+
+linksVariation.forEach(link => {
+    link.addEventListener('click', linkVariationHandle)
 })
