b3db50942ecf6c1ea7ff85217aed82e55650e996
Bartek0074 <88652468+Bartek0074@users.noreply.github.com>
2022-07-06 12:38:19 +0200

    added css code generator

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const generatorOpener = document.querySelector('.generator-opener')
 const generatorCloser = document.querySelector('.generator-closer')
 const generateCodeFrame = document.querySelector('.generator')
 const codeHtml = document.querySelector('.html-code')
+const codeCss = document.querySelector('.css-code')
 const stageLink = document.querySelector('.link')
 const root = document.querySelector(':root')
 const textInput = document.querySelector('#text')
@@ -99,10 +100,455 @@ const actualizeCode = () => {
             linkClass = link.classList[1]
         }
     })
-    
-    console.log(linkClass)
-
     codeHtml.value = `<a class="${linkClass}">${textInput.value}</a>`
+    
+    // CSS
+
+
+    // RESET FONT-WEIGHT AND FONT-STYLE VALUES
+    if (root.style.getPropertyValue('--font-weight') === ""){
+        root.style.setProperty('--font-weight', 'normal')
+    }
+    if (root.style.getPropertyValue('--font-style') === ""){
+        root.style.setProperty('--font-style', 'normal')
+    }
+
+    switch(linkClass) {
+        case 'hover__first':
+            codeCss.value = `.hover__first {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    cursor: pointer;
+    color: ${fontColorInputText.value};
+}
+            
+.hover__first,
+.hover__first::before,
+.hover__first::after {
+    transition: .3s;
+}
+
+.hover__first:hover {
+    color: ${mainColorInputText.value};
+}` 
+            break;
+        case 'hover__second':
+            codeCss.value = `.hover__second {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+    border-bottom: ${underlineHeightInput.value}px solid transparent;
+}
+            
+.hover__second,
+.hover__second::before,
+.hover__second::after {
+    transition: .3s;
+}
+
+.hover__second:hover {
+    color: ${mainColorInputText.value};
+}` 
+            break;
+       
+        case 'underline__first':
+            codeCss.value = `.underline__first {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+    border-bottom: ${underlineHeightInput.value}px solid transparent;
+}
+.underline__first:hover {
+    border-bottom-color: ${mainColorInputText.value};
+}` 
+            break;
+        case 'underline__second':
+            codeCss.value = `.underline__second {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.underline__second::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    transform: scaleX(0);
+    height: ${underlineHeightInput.value}px;
+    background-color: ${mainColorInputText.value};
+}  
+.underline__second:before {
+    transform: scaleX(1);
+}` 
+            break;
+        case 'underline__third':
+            codeCss.value = `.underline__third {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.underline__third::before,
+.underline__third::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    height: ${underlineHeightInput.value}px;
+    width: 51%;
+    transform: scaleX(0);
+    background-color: ${mainColorInputText.value};
+    transition: transform .3s;
+}  
+.underline__third::before {
+    left: 0;
+    transform-origin: left;
+}
+.underline__third::after {
+    right: 0;
+    transform-origin: right;
+}
+.underline__third:hover::before {
+    transform: scaleX(1);
+    transform-origin: right;
+} 
+.underline__third:hover::after {
+    transform: scaleX(1);
+    transform-origin: left;
+}` 
+            break;
+        case 'underline__fourth':
+            codeCss.value = `.underline__fourth {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.underline__fourth::before,
+.underline__fourth::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    height: ${underlineHeightInput.value}px;
+    width: 51%;
+    transform: scaleX(0);
+    background-color: ${mainColorInputText.value};
+    transition: transform .3s;
+}
+.underline__fourth::before {
+    left: 0;
+    transform-origin: right;
+}  
+.underline__fourth::after {
+    right: 0;
+    transform-origin: left;
+}
+.underline__fourth:hover::before {
+    transform: scaleX(1);
+    transform-origin: left;
+}  
+.underline__fourth:hover::after {
+    transform: scaleX(1);
+    transform-origin: right;
+}` 
+            break;
+        case 'underline__fifth':
+            codeCss.value = `.underline__fifth {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.underline__fifth::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    transform: scaleX(0);
+    transform-origin: left;
+    height: ${underlineHeightInput.value}px;
+    background-color: ${mainColorInputText.value};
+} 
+.underline__fifth:hover::before {
+    transform: scaleX(1);
+}` 
+            break;
+        case 'underline__sixth':
+            codeCss.value = `.underline__sixth {
+    position: relative;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.underline__sixth::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    transform: scaleX(0);
+    transform-origin: right;
+    height: ${underlineHeightInput.value}px;
+    background-color: ${mainColorInputText.value};
+    transition: transform .3s ease-in;
+}
+.underline__sixth:hover::before {
+    transform: scaleX(1);
+    transform-origin: left;
+}` 
+            break;
+    
+        case 'background__first':
+            codeCss.value = `.background__first {
+    position: relative;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__first:hover {
+    background-color: ${mainColorInputText.value};
+    color: ${bgcInputText.value});
+}` 
+            break;
+            break;
+        case 'background__second':
+            codeCss.value = `.background__second {
+    position: relative;
+    z-index: 0;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__second::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleY(0);
+    background-color: ${mainColorInputText.value};
+    z-index: -1;
+} 
+.background__second:hover::before {
+    transform: scaleY(1);
+}
+.background__second:hover {
+    color: ${bgcInputText.value};
+}` 
+            break;
+        case 'background__third':
+            codeCss.value = `.background__third {
+    position: relative;
+    z-index: 0;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__third::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleY(0);
+    transform-origin: bottom;
+    background-color: ${mainColorInputText.value};
+    z-index: -1;
+}
+.background__third:hover::before {
+    transform: scaleY(1);
+}
+.background__third:hover {
+    color: ${bgcInputText.value};
+}` 
+            break;
+        case 'background__fourth':
+            codeCss.value = `.background__fourth {
+    position: relative;
+    z-index: 0;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__fourth::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleY(0);
+    transform-origin: top;
+    background-color: ${mainColorInputText.value};
+    z-index: -1;
+    transition: transform .3s;
+} 
+.background__fourth:hover::before {
+    transform-origin: bottom;
+    transform: scaleY(1);
+} 
+.background__fourth:hover {
+    color: ${bgcInputText.value};
+}` 
+            break;
+        case 'background__fifth':
+            codeCss.value = `.background__fifth {
+    position: relative;
+    z-index: 0;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__fifth::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleX(0);
+    background-color: ${mainColorInputText.value};
+    z-index: -1;
+}
+.background__fifth:hover::before {
+        transform: scaleX(1);
+}
+.background__fifth:hover {
+    color: ${bgcInputText.value};
+}` 
+            break;
+        case 'background__sixth':
+            codeCss.value = `.background__sixth {
+    position: relative;
+    z-index: 0;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__sixth::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleX(0);
+    transform-origin: left;
+    background-color: ${mainColorInputText.value};
+    z-index: -1;
+}
+.background__sixth:hover::before {
+    transform: scaleX(1);
+}
+.background__sixth:hover {
+    color: ${bgcInputText.value};
+}` 
+            break;
+        case 'background__seventh':
+            codeCss.value = `.background__seventh {
+    position: relative;
+    z-index: 0;
+    padding: 10px 20px;
+    font-family: ${fontFamilyInput.value};
+    font-size: ${fontSizeInput.value}px;
+    font-weight: ${root.style.getPropertyValue('--font-weight')};
+    font-style: ${root.style.getPropertyValue('--font-style')};
+    text-decoration: none;
+    color: ${fontColorInputText.value};
+    cursor: pointer;
+}
+.background__seventh::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleX(0);
+    transform-origin: right;
+    background-color: ${mainColorInputText.value};
+    z-index: -1;
+    transition: transform .3s;
+}
+.background__seventh:hover::before {
+    transform: scaleX(1);
+    transform-origin: left;
+} 
+.background__seventh:hover {
+    color: ${bgcInputText.value};
+}` 
+            break;
+        default:
+            console.log('nie ma')
+      }
 }
 
 // TEXT VALUE OPTION
